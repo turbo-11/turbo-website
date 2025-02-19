2aed9362a0e0e231d6a23b1aea4f88b22648f9fa
Turbo <turboprogrammer1@gmail.com>
2025-02-19 12:25:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// add class active to header on scroll
+
+let header = document.querySelector("header")
+
+window.onscroll = function(){
+    if(this.scrollY >= 50){
+        header.classList.add("active")
+    }else{
+        header.classList.remove("active")
+    }
+}
+
+let nav_links = document.getElementById("links");
+
+function Open_close_Menu() {
+    nav_links.classList.toggle("active")
+}
